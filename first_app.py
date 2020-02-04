088d1bdf219f3e989ec9cd1e8d11bf3454b80dad
mustafacengiz <33909759+mustafacengiz@users.noreply.github.com>
2020-02-03 22:38:23 -0500

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -170,4 +170,4 @@ s = df.loc[df[df['SPLocation'] == beach].index[0]]
 s[2] = pd.datetime.now().month
 s[3] = (pd.datetime.now().month -1) * 30 + pd.datetime.now().day
 st.write('Our prediction for today is: ')
-st.text(model.predict(s.values.reshape(1, -1)[0])
+st.text(model.predict(s.values.reshape(1, -1)[0][1:]))
