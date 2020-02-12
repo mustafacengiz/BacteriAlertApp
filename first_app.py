3b74b1498720d315e879fce8b8a8a27d4aa95a15
mustafacengiz <33909759+mustafacengiz@users.noreply.github.com>
2020-02-11 19:15:28 -0500

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -6,7 +6,18 @@ import pandas as pd
 import pickle
 import sklearn
 from sklearn.linear_model import LogisticRegression
-
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+   
 st.title('Welcome to BacteriAlert')
 st.write("This app provides bacterial beach water pollution predictions for 137 beaches in 26 costal Florida counties within seven days. Our predictions are based on Florida Health Department's beach water test data during 2000-2017.")
 #st.write( "Our predictions are based on Florida Health Department's historical beach water test data.")
@@ -230,7 +241,10 @@ if beach != ' ':
 #abc
 
 
+print(color.BOLD + 'Hello World !' + color.END)
+
  st.write("Here are our predictions for "+beach+" for next seven days.")
+ 
 #import datetime as dt
  for x in range(7):
  #t = pd.dt.today() + DT.timedelta(days=x)
@@ -259,4 +273,4 @@ if beach != ' ':
 #st.write('Our prediction for tomorrow is: ')
  #st.text(model.predict(t[1:].values.reshape(1, -1)))
 
-st.write('Note: The pollution level is determined by bacteria level. "Polluted" means that the enterococcus level is expected to be greater than 70 colony per 100 ml at the given location on the given date.')
+ st.write((color.BOLD + 'Note: ' + color.END)+ 'The pollution level is determined by bacteria level. "Polluted" means that the enterococcus level is expected to be greater than 70 colony per 100 ml at the given location on the given date.')
